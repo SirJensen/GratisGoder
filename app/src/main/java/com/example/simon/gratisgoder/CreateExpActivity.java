1c843140bb9fb8c8c9d365f6c95cf1b284c34d78
JonesBawz <jonasfrii94@gmail.com>
2017-11-07 16:32:12 +0100

    Updated compileSdkVersion, buildToolsVersion, targetSdkVersion, and support library version to version 26 in the build.gradle

---
+++ app/src/main/java/com/example/simon/gratisgoder/CreateExpActivity.java
@@ -0,0 +1,17 @@
+package com.example.simon.gratisgoder;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jonas on 07-11-2017.
+ */
+
+public class CreateExpActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createexp);
+    }
+
+}

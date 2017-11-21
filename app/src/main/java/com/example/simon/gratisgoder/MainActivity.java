2f02b290e933ccd058cb21c0c3904e7223dc0b22
JonesBawz <jonasfrii94@gmail.com>
2017-11-22 00:12:18 +0100

    Crashlytics kører

--- app/src/main/java/com/example/simon/gratisgoder/MainActivity.java
+++ app/src/main/java/com/example/simon/gratisgoder/MainActivity.java
@@ -3,11 +3,10 @@ package com.example.simon.gratisgoder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         new Handler().postDelayed(new Runnable() {
